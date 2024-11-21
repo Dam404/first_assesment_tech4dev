5b66738244f5d79171f375782390c885c9710cb2
Odeyemi Damilola <odeyemidamilola78@gmail.com>
2024-11-21 09:28:54 +0100

    my first commit to this program

---
+++ folder/main.js
@@ -0,0 +1,16 @@
+// const users = [
+//     {name: 'Damilola', email: 'damilola@gmail.com', password: ''},
+//     {name: 'Tolu', email: 'Tolu@gmail.com', password: ''},
+// ]
+
+// for (const user of users){
+//     console.log(user.name);
+//     console.log(user.email);
+// }
+
+let buttonClicks = 0;
+
+while (buttonClicks < 10){
+    buttonClicks++;
+    console.log('Button clicked', buttonClicks);
+}
